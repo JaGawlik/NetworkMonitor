1e1339ddb2a7e5b7b3343b9d9f09cecfcc7fd999
JaGawlik <92257357+JaGawlik@users.noreply.github.com>
2024-12-06 17:14:13 +0100

    Snort alert to database

--- NetworkMonitor/App.xaml.cs
+++ NetworkMonitor/App.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System.Configuration;
 using System.Data;
+using System.Diagnostics;
+using System.IO;
 using System.Windows;
 using NetworkMonitor.Database;
 
@@ -22,6 +24,19 @@ namespace NetworkMonitor
             string targetDb = "ids_system";
 
             string snortLogPath = @"C:\Snort\log\alert.ids";
+            string snortPath = @"C:\Snort\bin\snort.exe";
+            string arguments = "-i 6 -c C:\\Snort\\etc\\snort.conf -l C:\\Snort\\log -A fast -N";
+
+            bool snortStartded = Snort.SnortManager.StartSnort(snortPath, arguments);
+
+            if (!snortStartded)
+            {
+                MessageBox.Show("Nie udało się uruchomić Snorta. Aplikacja zostanie zamknięta.");
+                Application.Current.Shutdown();
+                return;
+            }
+
+
 
             //Connection do bazowej bazy
             string initialConnectionString = $"Host={host};Port={port};Username={user};Password={password};Database={initialDatabase}";
---
+++ NetworkMonitor/Snort/SnortManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace NetworkMonitor.Snort
+{
+    internal class SnortManager
+    {
+        public static bool StartSnort(string snortPath, string arguments)
+        {
+            if (!File.Exists(snortPath))
+            {
+                MessageBox.Show("Nie znaleziono pliku snort.exe w podanej lokalizacji. Aplikacja zostanie zamknięta.");
+                Application.Current.Shutdown();
+                return false;
+            }
+
+            var startInfo = new ProcessStartInfo()
+            {
+                FileName = snortPath,
+                Arguments = arguments,
+                UseShellExecute = false,
+                CreateNoWindow = true,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true
+            };
+
+            Process snortProcess = new Process();
+            snortProcess.StartInfo = startInfo;
+
+            bool started = snortProcess.Start();
+            if (started)
+            {
+                Console.WriteLine("Snort został uruchomiony.");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Nie udało się uruchomić Snorta.");
+                return false;
+            }
+        }
+    
+    }
+}
